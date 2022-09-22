a5249daa55cc560bed8ef814168038799eceb621
tejeda418 <114043816+tejeda418@users.noreply.github.com>
2022-09-21 21:12:00 -0600

    Agregar archivos de proyecto.

---
+++ clresta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opoeracion1
+{
+    internal class clresta
+    {
+        public clresta(int n1, int n2)
+        {
+            N1 = n1;
+            N2 = n2;
+        }
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+
+        public int restar()
+        {
+            return N1 - N2;
+        }
+    }
+}
